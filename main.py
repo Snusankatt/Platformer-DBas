8e303f2e2f0f535b1c29f58286a8b8e93500f70d
Snusankatt <victor.rydegran@gmail.com>
2026-09-25 17:03:26 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import player
+import pygame
+import sys
+
+def main():
+    # Starta pygame med ett fönster, sätt storlek och skapa klockan
+    pygame.init()
+    screen = pygame.display.set_mode((1920, 1080))
+    clock = pygame.time.Clock()
+
+    # Skapa spelaren
+    p = player.Player()
+
+    while True:
+        clock.tick(60)
---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self):
+        self.posX = 0
+        self.posY = 0
+        self.velX = 0
+        self.velY = 0
+        self.accX = 0
+        self.accY = 0
+
+        self.isAlive = True
